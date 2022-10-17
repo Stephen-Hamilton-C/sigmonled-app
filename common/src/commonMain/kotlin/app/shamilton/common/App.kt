d76331c18af0577cb2b36e853038c79761f04d08
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2022-10-17 19:05:51 -0400

    Messing with MaterialTheme

--- common/src/commonMain/kotlin/app/shamilton/common/App.kt
+++ common/src/commonMain/kotlin/app/shamilton/common/App.kt
@@ -1,21 +1,61 @@
 package app.shamilton.common
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun App() {
+    val lightColors = lightColors(
+        primary = Color(0xFF1EB980)
+    )
+
+    val darkColors = darkColors(
+        primary = Color(0xFF66ffc7)
+    )
+
+    val colors = if (isSystemInDarkTheme()) darkColors else lightColors
+
+    val typography = Typography(
+        h1 = TextStyle(
+            fontWeight = FontWeight.W100,
+            fontSize = 96.sp
+        ),
+        button = TextStyle(
+            fontWeight = FontWeight.W600,
+            fontSize = 14.sp
+        )
+    )
+
+
+
     var text by remember { mutableStateOf("Hello, World!") }
     val platformName = getPlatformName()
 
-    Button(onClick = {
-        text = "Hello, ${platformName}"
-    }) {
-        Text(text)
+    MaterialTheme(colors, typography) {
+        Column() {
+            Button(onClick = {
+                text = "Hello, $platformName"
+            }) {
+                Text(text)
+            }
+            Text("Hello")
+        }
     }
 }
